f66dbf3af3188a20b30a134e115e3360344d167e
AchmadGozali8 <achmadg6@gmail.com>
2016-05-12 23:35:29 +0700

    Default

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'application' => 
+  array (
+    'debug' => false,
+  ),
+  'database' => 
+  array (
+    'default' => 'sqlite',
+    'connections' => 
+    array (
+      'sqlite' => 
+      array (
+        'prefix' => 'pk_',
+      ),
+    ),
+  ),
+  'system' => 
+  array (
+    'secret' => 'BD02saF3EoAyV0K9Z.wohh6jPIX7q/AQn9SWDrN8IaHnD/YG/1FUOPLxhDPGIB0w',
+  ),
+);
